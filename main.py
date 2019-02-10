498cb012572be619d5f96a5f00ccf6eb6b9b7a2f
Makarand Deshpande Ubuntu <makarand_deshpande@outlook.com>
2019-02-10 14:05:06 +0530

    adds banana env

--- main.py
+++ main.py
@@ -1,6 +1,142 @@
-import gym
 import random
 import torch
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
+from unityagents import UnityEnvironment
+
+from agent import Agent
+
+# please do not modify the line below
+env = UnityEnvironment(file_name="./Banana_Linux/Banana.x86_64")
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+
+    # reset the environment
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents in the environment
+    print('Number of agents:', len(env_info.agents))
+
+    # number of actions
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+
+    # examine the state space 
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    
+    state_size = len(state)
+    print('States have length:', state_size)
+
+    agent = Agent(state_size=37, action_size=4, seed=0)
+    average_score = dict()             # average score list containing avg score every 100 episode
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+
+    for i_episode in range(1, n_episodes+1):
+
+        env_info = env.reset(train_mode=True)[brain_name]  # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize the score
+
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            avg_score = np.mean(scores_window)
+            average_score[i_episode] = avg_score
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+def play(n_episodes=5, max_t=1000, eps_start=0, eps_end=0.00, eps_decay=0):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+
+    # reset the environment
+    env_info = env.reset(train_mode=False)[brain_name]
+
+    # number of agents in the environment
+    print('Number of agents:', len(env_info.agents))
+
+    # number of actions
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+
+    # examine the state space 
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    
+    state_size = len(state)
+    print('States have length:', state_size)
+
+    agent = Agent(state_size=37, action_size=4, seed=0)
+    state_dict = torch.load('checkpoint.pth')
+    agent.qnetwork_local.load_state_dict(state_dict)
+    eps = eps_start                    # initialize epsilon
+
+    for i_episode in range(1, n_episodes+1):
+
+        env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize the score
+
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            state = next_state
+            score += reward
+            if done:
+                break
+
+        print('\rEpisode {}\t Score: {:.2f}'.format(i_episode, score))
+
+
+def plot_scores(scores, average_score): 
+    pass
+
+play()
+env.close()
